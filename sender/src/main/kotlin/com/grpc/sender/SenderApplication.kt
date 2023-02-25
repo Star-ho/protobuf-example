f4615e68176825eaf622ec24f75a0b9c8bdf2d89
sungho <tjdgh1231@tpirates.com>
2023-02-25 21:31:59 +0900

    first commit!

---
+++ sender/src/main/kotlin/com/grpc/sender/SenderApplication.kt
@@ -0,0 +1,11 @@
+package com.grpc.sender
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SenderApplication
+
+fun main(args: Array<String>) {
+    runApplication<SenderApplication>(*args)
+}
