94ea99d3eed8a4e02be583a0e6e3d11e1873dd20
sungho <tjdgh1231@tpirates.com>
2023-02-25 21:30:09 +0900

    first commit!

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.0.3"  apply false
+    id("io.spring.dependency-management") version "1.1.0" apply false
+    kotlin("jvm") version "1.7.22" apply false
+    kotlin("plugin.spring") version "1.7.22" apply false
+}
